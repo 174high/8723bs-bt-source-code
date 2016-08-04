aff21d4b3a20eb9547ef43a511603485b8d0c900
johnny <466243346@qq.com>
2016-08-04 16:23:23 +0800

    add change

--- enable_bt/start-bt.sh
+++ enable_bt/start-bt.sh
@@ -5,7 +5,7 @@ do
     var2=`ps -A | grep "rtk_hciattach" | cut -c 2-5`
     sudo kill -9 $var2
     sudo rmmod enable_rtl8723bs_bt
-    sudo insmod /home/k/Desktop/enable_bt/enable_rtl8723bs_bt.ko
+    sudo insmod /home/k/8723bs-bt-source-code/enable_bt/enable_rtl8723bs_bt.ko
     sudo rtk_hciattach -n -s 115200 ttyS4 rtk_h5 > hciattach.txt 2>&1 &
     sleep 5
     var1=`hciconfig -a| grep "Manufacturer"`
